937b4e07779bd24ee5efb24ec82eb290fe9ee8c1
mostafa.ma.saleh@gmail.com <Maxi1Rules2All!>
2017-03-25 15:29:24 +0200

    Updated Google Play Services to 10.2.1

--- app/src/main/java/com/iti/tripplanner/BootReceiver.java
+++ app/src/main/java/com/iti/tripplanner/BootReceiver.java
@@ -13,24 +13,25 @@ import com.google.firebase.database.ValueEventListener;
 public class BootReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(final Context context, Intent intent) {
-        DBAdapter.getDatabase()
-                .getReference(FirebaseAuth.getInstance().getCurrentUser().getUid())
-                .addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-                        for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
-                            if (!postSnapshot.getKey().equals("Count")) {
-                                Trip trip = postSnapshot.getValue(Trip.class);
-                                if (trip.getTimeInMillis() > System.currentTimeMillis() && !trip.isDone())
-                                    trip.setAlarm(context);
+        if (FirebaseAuth.getInstance().getCurrentUser() != null)
+            DBAdapter.getDatabase()
+                    .getReference(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                    .addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+                                if (!postSnapshot.getKey().equals("Count")) {
+                                    Trip trip = postSnapshot.getValue(Trip.class);
+                                    if (trip.getTimeInMillis() > System.currentTimeMillis() && !trip.isDone())
+                                        trip.setAlarm(context);
+                                }
                             }
                         }
-                    }
 
-                    @Override
-                    public void onCancelled(DatabaseError databaseError) {
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
 
-                    }
-                });
+                        }
+                    });
     }
 }
